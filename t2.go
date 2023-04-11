cfa7ac90f8f06c55875cfce8e4b55a4afaad19a6
caroljoyv <caroljoyv27@gmail.com>
2023-04-11 22:44:29 +0530

    added t1.go and t2.go

---
+++ t2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var f_name string = "Carolene"
+	var l_name string
+	var number uint8 = 30
+
+	l_name = "Joy"
+
+	fmt.Println(f_name, l_name, number)
+
+}
